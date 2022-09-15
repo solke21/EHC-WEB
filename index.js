d4fcb3403f86f6e7f9ad1dcbb1fe125d9792806c
sol <ssuarezf84@gmail.com>
2022-09-15 18:58:29 +0200

    install dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('server running');
 })
