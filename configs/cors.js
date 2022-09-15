c0032ee919c9b55083203c6715e850b6aa190077
Sol Suárez <92597259+solke21@users.noreply.github.com>
2022-09-15 21:16:17 +0200

    incluyed sql, docker-compose & configs (#2)

---
+++ configs/cors.js
@@ -0,0 +1,12 @@
+const whitelist = ["http://localhost:3006"];
+
+module.exports = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error());
+        }
+    },
+    credentials: true,
+};
---
+++ sql/insertion.sql
@@ -0,0 +1,4 @@
+INSERT INTO events (
+    name_event, price, details, club_event
+) VALUES (
+    'EHM 30/09 RE-Opening', 15, '00:00h - 06:00h pm @daviddorado @johnjumar @raulvilar @solke - special guest hueli&muli', '@hangar48');
